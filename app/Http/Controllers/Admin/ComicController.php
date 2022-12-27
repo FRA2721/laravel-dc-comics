824cad09b0c4dfc51df679d6ed8a5ed8ad5d7677
FRA2721 <cimino.francesco36@gmail.com>
2022-12-27 20:12:56 +0100

    store controller completed

--- app/Http/Controllers/Admin/ComicController.php
+++ app/Http/Controllers/Admin/ComicController.php
@@ -40,7 +40,11 @@ class ComicController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $data = $request->all();
+        $newComic = new Comic();
+        $newComic->fill($data);
+        $newComic->save();
+        return redirect()->route('comics.show', $newComic->id);
     }
 
     /**
